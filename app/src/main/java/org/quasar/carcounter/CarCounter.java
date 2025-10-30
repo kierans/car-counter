c400524660710d7f7d5eb6c64c9d149f937657a4
Kieran Simpson <kierans777@gmail.com>
2025-10-30 12:22:50 +1100

    Refactor to use a Heap for O(log) time

--- app/src/main/java/org/quasar/carcounter/CarCounter.java
+++ app/src/main/java/org/quasar/carcounter/CarCounter.java
@@ -1,17 +1,16 @@
 package org.quasar.carcounter;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 
 import org.jetbrains.annotations.NotNull;
 
 /*
  * The implementations of the methods are not optimized for performance.
- *
- * For large data sets, the methods should use more efficient data structures like trees to reduce the linear complexity
- * to logarithmic complexity.
  */
 public class CarCounter {
   private final List<Period> periods;
@@ -38,10 +37,10 @@ public class CarCounter {
   }
 
   public List<Period> topUsagePeriods() {
-    return periods.stream()
-      .sorted(Comparator.comparing(Period::numCars, Comparator.reverseOrder()))
-      .collect(Collectors.toList())
-      .subList(0, 3);
+    // if frequently accessed, this could be an attribute.
+    final PriorityQueue<Period> heap = indexCarsByTopUsage();
+
+    return take(3, heap);
   }
 
   /**
@@ -62,5 +61,43 @@ public class CarCounter {
     public String toFormat() {
       return "%s %s".formatted(date, cars);
     }
+  }
+
+  /*
+   * Indexes the periods by the number of cars they contain.
+   *
+   * The result is a sorted tree that has O(log(n)) insertion and retrieval time.
+   * For large data sets this is an example of where a tree is more efficient than a simple list reduction.
+   */
+  private @NotNull PriorityQueue<Period> indexCarsByTopUsage() {
+    final PriorityQueue<Period> heap = new PriorityQueue<>(
+      this.periods.size(),
+      Comparator.comparing(Period::numCars, Comparator.reverseOrder())
+    );
+
+    heap.addAll(periods);
+
+    return heap;
+  }
+
+  private static @NotNull List<Period> take(final int n, final PriorityQueue<Period> heap) {
+    final List<Period> result = new ArrayList<>();
+
+    if (n <= 0) {
+      throw new IllegalArgumentException("n must be greater than 0");
+    }
+
+    if (heap.size() < n) {
+      throw new IllegalArgumentException("n must be less than or equal to the number of periods");
+    }
+
+    /*
+     * Because the PriorityQueue streaming doesn't guarantee the order of the elements, we need to poll them in order.
+     */
+    for (int i = 0; i < n;  i++) {
+      result.add(heap.poll());
+    }
+
+    return result;
   }
 }
